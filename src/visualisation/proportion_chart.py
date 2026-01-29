26b0e994a4158f186eec596760915973bf61594c
gall <daniel.gallagher.js@gmail.com>
2026-01-29 16:35:40 +0100

    removed old code

--- src/visualisation/proportion_chart.py
+++ src/visualisation/proportion_chart.py
@@ -1,100 +1,105 @@
 import pandas as pd
 from plotnine import (
+    element_text,
+    element_line,
+    coord_flip,
     ggplot,
     aes,
-    geom_jitter,
     geom_boxplot,
-    after_stat,
-    stage,
-    geom_bar,
-    geom_text,
-    geom_label,
-    position_dodge2,
-    position_dodge,
+    scale_fill_manual,
+    scale_color_manual,
     facet_wrap,
-    geom_col,
     labs,
-    scale_y_continuous,
-    position_stack
+    theme, 
+    element_rect,
 )
-from plotnine.data import mtcars
+
+EVAL_RESULTS_PATH = "./remote/evaluation_results"
+RESULTS_REFORMATTED_PATH = f"{EVAL_RESULTS_PATH}/vis/bar-chart-reformatted.csv"
+VIS_OUTPUT_PATH = "./vis/results-bar-chart.png"
 
 tasks = {
-    "token-level-intransitive-nom-subj": "Nominative (Subj)",
-    "token-level-transitive-nom-dat-subj": "Nominative-Dative (Subj)",
-    "token-level-transitive-nom-dat-obj": "Nominative-Dative (Obj)",
-    "token-level-transitive-erg-nom-subj": "Ergative-Nominative (Subj)",
-    "token-level-transitive-erg-nom-obj": "Ergative-Nominative (Obj)",
-    "token-level-transitive-dat-nom-subj": "Dative-Nominative (Subj)",
-    "token-level-transitive-dat-nom-obj": "Dative-Nominative (Obj)",
+    "token-level-intransitive-nom-subj": "INTRANSITIVE NOMINATIVE (SUBJECT)",
+
+    "token-level-transitive-nom-dat-subj": "NOMINATIVE - DATIVE (SUBJECT)",
+    "token-level-transitive-nom-dat-obj": "NOMINATIVE - DATIVE (OBJECT)",
+
+    "token-level-transitive-erg-nom-subj": "ERGATIVE - NOMINATIVE (SUBJECT)",
+    "token-level-transitive-erg-nom-obj": "ERGATIVE - NOMINATIVE (OBJECT)",
+
+    "token-level-transitive-dat-nom-subj": "DATIVE - NOMINATIVE (SUBJECT)",
+    "token-level-transitive-dat-nom-obj": "DATIVE - NOMINATIVE (OBJECT)",
 }
 
-final_df = pd.DataFrame()
+results_reformatted = pd.DataFrame()
+
 for task in tasks.keys():
-    results_df = pd.read_csv(f"../../evaluation_results_remote/evaluation_results/means/{task}.csv")
-    print(results_df)
-    long_df = results_df.melt(id_vars=["model","repo"], var_name="form", value_name="prob")
+    results_path_task = f"{EVAL_RESULTS_PATH}/means/{task}.csv"
+    task_results_df = pd.read_csv(results_path_task)
+
+    print("====Creating Proportion Chart====")
+    print(task_results_df.head())
+
+    long_df = task_results_df.melt(id_vars=["model","repo"], var_name="form", value_name="prob")
     long_df["form"] = long_df["form"].apply(lambda x: x.replace("form_grammatical_","").replace("form_ungrammatical_","").upper())
     long_df["task"] = tasks[task]
 
-    final_df = pd.concat([final_df, long_df])
+    results_reformatted = pd.concat([results_reformatted, long_df])
 
-final_df.to_csv("final.csv")
-final_df["form"] = pd.Categorical(
-    final_df["form"],
+
+results_reformatted.to_csv(RESULTS_REFORMATTED_PATH)
+
+results_reformatted["form"] = pd.Categorical(
+    results_reformatted["form"],
     categories=["NOM","ERG","DAT"],
     ordered=True
 )
-final_df["task"] = pd.Categorical(
-    final_df["task"],
+results_reformatted["task"] = pd.Categorical(
+    results_reformatted["task"],
     categories=tasks.values(),
     ordered=True
 )
 
+colors = {
+    "NOM": "#f2710e",  
+    "ERG": "#5c30b5", 
+    "DAT": "#0e93f2",
+}
+
+print("Results reformatted for bar chart output:")
+print(results_reformatted.head())
+
 p = (
-    ggplot(final_df, aes(x="form", y="prob", color="form"))
-    # + geom_jitter(width=0.2, size=3, alpha=0.7)  # each point = one model
-    + geom_boxplot(aes(fill="form"), alpha=0.3, outlier_shape=None)  # optional, summary
-    + facet_wrap("~ task", nrow=1)
-    + scale_y_continuous(labels=lambda l: [f"{v:.0%}" for v in l])
-    + labs(y="Proportion assigned", x="Form")
+    ggplot(results_reformatted, aes(x="form", y="prob", color="form"))
+    + geom_boxplot(aes(fill="form"), alpha=0.8, color="black", outlier_shape=None)  
+    + facet_wrap("~ task", ncol=1)
+    + coord_flip()
+    + labs(y="Average Model Probability", x="Form", fill="Form")
+    + scale_fill_manual(values=colors)
+    + scale_color_manual(values=colors)
+    + theme(
+        panel_background=element_rect(fill="white", color=None),
+        strip_background=element_rect(fill="white", color=None),
+        plot_background=element_rect(fill="white", color=None),
+        # Keep axes
+        axis_line=element_line(color="black"),
+        axis_ticks=element_line(color="black"),
+
+        # Text clarity
+        axis_text=element_text(size=11),
+        axis_title=element_text(size=13, weight="bold"),
+        strip_text=element_text(size=12, weight="bold"),
+        legend_title=element_text(size=12, weight="bold"),
+        legend_text=element_text(size=11),
+
+        axis_text_x=element_text(size=12, color="black"),
+        axis_text_y=element_text(size=12, color="black"),
+    )
 )
 
 p.save(
-    "../../vis/proportion_plot.png",
-    width=18,   
-    height=6,
+    VIS_OUTPUT_PATH,
+    width=5,   
+    height=15,
     dpi=300
 )
-
-"""
-# OLD
-grouped_df = final_df.groupby(["task","form"], as_index=False)["prob"].mean()
-grouped_df["pct"] = grouped_df.groupby(["task"], as_index=False)["prob"].transform(lambda x: x / x.sum())
-grouped_df["pct_label"] = grouped_df["pct"].map(lambda x: f"{x:.2%}")
-
-grouped_df["form"] = pd.Categorical(
-    grouped_df["form"],
-    categories=["NOM","ERG","DAT"],
-    ordered=True
-)
-grouped_df["task"] = pd.Categorical(
-    grouped_df["task"],
-    categories=tasks.values(),
-    ordered=True
-)
-
-p = (
-    ggplot(grouped_df, aes(x="form", y="pct", fill="form"))
-    + geom_col()
-    + geom_jitter(width=0.2, size=3, alpha=0.7)
-    + geom_text(
-        aes(label="pct_label"),
-        va="bottom",
-        size=9
-    )
-    + facet_wrap("~ task", nrow=1)
-    + scale_y_continuous(labels=lambda l: [f"{v:.0%}" for v in l])
-    + labs(y="Percentage", x="Form")
-)
-"""
